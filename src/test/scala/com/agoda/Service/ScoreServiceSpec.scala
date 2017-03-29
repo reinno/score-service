3ac5540e0f064e409d0e468be9e44fc285ad47db
wuzhonglin <wuzhonglin.cn@gmail.com>
2017-03-29 17:22:04 +0800

    add rule service switch test case

--- src/test/scala/com/agoda/Service/ScoreServiceSpec.scala
+++ src/test/scala/com/agoda/Service/ScoreServiceSpec.scala
@@ -1,5 +1,6 @@
 package com.agoda.Service
 
+import akka.http.scaladsl.model.StatusCodes
 import com.agoda.Setting
 import com.agoda.model.Rule
 import com.agoda.route.ScoreRoute
@@ -27,9 +28,43 @@ class ScoreServiceSpec extends BaseServiceHelper.TestSpec {
       system.stop(scoreService)
     }
 
+    val countryRuleName = Constants.Rules.countryRule
     "return single value when single rule enabled" in {
-      val rules = Map(Constants.Rules.countryRule -> Rule(Constants.Rules.countryRule, 5))
+      val rules = Map(countryRuleName -> Rule(countryRuleName, 5))
       val scoreService = system.actorOf(ScoreService.props(Setting(rules = rules)))
+
+      scoreService ! ScoreService.GetScoreRequest(List(ScoreRoute.ScoreRequest(1, 1)))
+      expectMsg(List(ScoreRoute.ScoreResponse(1, 5)))
+
+      system.stop(scoreService)
+    }
+
+    "return single value when single rule disabled" in {
+      val rules = Map(countryRuleName -> Rule(countryRuleName, 5, enabled = false))
+      val scoreService = system.actorOf(ScoreService.props(Setting(rules = rules)))
+
+      scoreService ! ScoreService.GetScoreRequest(List(ScoreRoute.ScoreRequest(1, 1)))
+      expectMsg(List(ScoreRoute.ScoreResponse(1, 0)))
+
+      system.stop(scoreService)
+    }
+
+
+    "return single value when single rule enable flag switch" in {
+      val rules = Map(countryRuleName -> Rule(countryRuleName, 5, enabled = true))
+      val scoreService = system.actorOf(ScoreService.props(Setting(rules = rules)))
+
+      scoreService ! ScoreService.GetScoreRequest(List(ScoreRoute.ScoreRequest(1, 1)))
+      expectMsg(List(ScoreRoute.ScoreResponse(1, 5)))
+
+      scoreService ! ScoreService.Disable(countryRuleName)
+      expectMsg(StatusCodes.OK)
+
+      scoreService ! ScoreService.GetScoreRequest(List(ScoreRoute.ScoreRequest(1, 1)))
+      expectMsg(List(ScoreRoute.ScoreResponse(1, 0)))
+
+      scoreService ! ScoreService.Enable(countryRuleName)
+      expectMsg(StatusCodes.OK)
 
       scoreService ! ScoreService.GetScoreRequest(List(ScoreRoute.ScoreRequest(1, 1)))
       expectMsg(List(ScoreRoute.ScoreResponse(1, 5)))
